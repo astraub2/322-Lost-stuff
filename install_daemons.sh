3d7fbd72af82505f56440d36b6d5ea2bf7500ba6
astraub2 <astraub2@uoregon.edu>
2017-01-16 14:24:19 -0800

    fix #5

--- install_daemons.sh
+++ install_daemons.sh
@@ -1,14 +1,4 @@
 cd ..
-curl -O http://mirrors.gigenet.com/apache//httpd/httpd-2.4.25.tar.gz
-gunzip httpd-2.4.25.tar.gz
-tar xf httpd-2.4.25.tar
-cd httpd-2.4.25
-./configure
-mkdir build_dir
-make
-make install DESTDIR=/home/osnapdev
-
-cd ..
 git clone https://github.com/postgres/postres.git
 cd postgres
 git checkout REL9_5_STABLE
@@ -18,4 +8,13 @@ cd build_dir
 cd ..
 make
 make install DESTDIR=/home/osnapdev
+
 cd ..
+curl -O http://mirrors.gigenet.com/apache//httpd/httpd-2.4.25.tar.gz
+gunzip httpd-2.4.25.tar.gz
+tar xf httpd-2.4.25.tar
+cd httpd-2.4.25
+./configure
+mkdir build_dir
+make
+make install DESTDIR=/home/osnapdev
