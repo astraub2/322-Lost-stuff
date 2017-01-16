4256843ba84abfbe86c8cc599b9206644111556d
astraub2 <astraub2@uoregon.edu>
2017-01-16 13:12:45 -0800

    The Appache and Porstgres installs

--- install_daemons.sh
+++ install_daemons.sh
@@ -1,4 +1,4 @@
-//Appache
+
 curl -o http://mirrors.sonic.net/apache//httpd/httpd-2.4.25.tar.gzcurl
 gunzip (thing)
 tar xf (thing)
@@ -10,7 +10,7 @@ cd ..
 make
 make install DESTDIR=/home/osnapdev
 
-//postgres
+
 git clone https://github.com/postgres/postres.git
 cd postgres
 git checkout REL9_5_STABLE
