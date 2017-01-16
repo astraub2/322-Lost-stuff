1923dda2e75ed03b7ac7210111003c65623cb912
astraub2 <astraub2@uoregon.edu>
2017-01-16 14:03:45 -0800

    maybe it will work now

--- install_daemons.sh
+++ install_daemons.sh
@@ -1,16 +1,14 @@
 
-curl -o http://mirrors.sonic.net/apache//httpd/httpd-2.4.25.tar.gzcurl
-gunzip (thing)
-tar xf (thing)
-(new dir) Appache
+curl -O http://mirrors.gigenet.com/apache//httpd/httpd-2.4.25.tar.gz
+gunzip httpd-2.4.25.tar.gz
+tar xf httpd-2.4.25.tar
+cd httpd-2.4.25
 ./configure
 mkdir build_dir
-cd build_dir
-cd ..
 make
 make install DESTDIR=/home/osnapdev
 
-
+cd ..
 git clone https://github.com/postgres/postres.git
 cd postgres
 git checkout REL9_5_STABLE
@@ -20,3 +18,4 @@ cd build_dir
 cd ..
 make
 make install DESTDIR=/home/osnapdev
+cd ..
