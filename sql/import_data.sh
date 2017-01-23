e76e0150f721d4220ff5d963b448bda642ca16ef
astraub2 <astraub2@uoregon.edu>
2017-01-23 13:41:52 -0800

    added python script

--- sql/import_data.sh
+++ sql/import_data.sh
@@ -4,9 +4,5 @@ initdb -D $HOME/data
 pg_ctl -D $HOME/data -l $HOME/logfile start
 createdb lost_db
 psql -d lost_db -a -f create_tables.sql
-
-cd ..
-cd ..
-curl -O https://classes.cs.uoregon.edu//17W/cis322/files/osnap_legacy.tar.gz
-tar -xvzf osnap_legacy.tar.gz
-//run the python script
+path=/osnap_legacy
+python3 add_stuff.py
