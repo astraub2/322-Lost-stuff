a8c28b5b03fda3f54a7240b5e66b940cdf9275f2
astraub2 <astraub2@uoregon.edu>
2017-01-23 12:21:45 -0800

    fix

--- sql/import_data.sh
+++ sql/import_data.sh
@@ -1,12 +1,12 @@
 //get the legacy data  using curl, unzip it etc
-$HOME
 mkdir data
 initdb -D $HOME/data
-pg_ctl -D $HOME/data -l $HOME/db_log.txt start
+pg_ctl -D $HOME/data -l $HOME/logfile start
 createdb lost_db
 psql -d lost_db -a -f create_tables.sql
 
-$HOME
+cd ..
+cd ..
 curl -O https://classes.cs.uoregon.edu//17W/cis322/files/osnap_legacy.tar.gz
 tar -xvzf osnap_legacy.tar.gz
 //run the python script
