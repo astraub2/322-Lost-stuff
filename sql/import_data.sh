84da31a651f66d2545fa5888dc6d46784827ae72
astraub2 <astraub2@uoregon.edu>
2017-01-26 19:47:56 -0800

    testing shell script

--- sql/import_data.sh
+++ sql/import_data.sh
@@ -2,9 +2,9 @@
 mkdir data
 initdb -D $HOME/data
 pg_ctl -D $HOME/data -l $HOME/logfile start
-createdb '$1'
-psql -d '$1' -a -f create_tables.sql
+createdb lost
+psql -d lost -a -f create_tables.sql
 cd osnap_legacy
 
 python3 ../add_final.py >output.sql
-psql -d lost_db -a -f output.sql
+psql -d lost -a -f output.sql
