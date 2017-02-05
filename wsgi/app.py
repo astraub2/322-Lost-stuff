a9bcc0e8708017f32a198770b00d570674f901e6
astraub2 <astraub2@uoregon.edu>
2017-02-05 12:02:29 -0800

    figured out input output

--- wsgi/app.py
+++ wsgi/app.py
@@ -1,5 +1,5 @@
 from flask import Flask,make_response, render_template, session, redirect, url_for, escape, request
-
+#import psycopg2
 app = Flask(__name__)
 #app.run(host='0.0.0.0', port=8080)
 #app.debug = False
@@ -31,7 +31,8 @@ def gettrans():
 @app.route('/getcookie')
 def getcookie():
    name = request.cookies.get('userID')
-   return '<h1>Username: '+name+'</h1>' '<br><button type="button" onclick="javascript:history.back()">Back</button>'
+   return ('<h1>Username: '+name+
+    '</h1>' '<br><button type="button" onclick="javascript:history.back()">Back</button>')
    
 @app.route('/login')
 def loginagain():
@@ -44,6 +45,7 @@ def intransrep():
        
        resp = make_response(render_template('intransrep.html'))
        resp.set_cookie('tdate', tdate)
+       transbuilder(tdate);
    
     return resp
     
@@ -55,8 +57,40 @@ def inventoryrep():
        resp = make_response(render_template('inventoryrep.html'))
        resp.set_cookie('idate', idate)
        resp.set_cookie('facility', facility)
+       invenbuilder(idate, facility)
    
     return resp
+
+
+def transbuilder(tdate):
+    #command="SELECT * FROM transit WHERE date=%s"(tdate)
+    try:
+        conn = psycopg2.connect("dbname='lost' user='osnapdev' host='localhost'")
+    except:
+        print ("I am unable to connect to the database")
+    cur = conn.cursor()
+    cur.execute("""SELECT * FROM transit""")
+    rows = cur.fetchall()
+    for row in rows:
+        print ("   ", row[0])
+
+
+
+
+def invenbuilder(idate, facility):
+    #command="SELECT * FROM transit WHERE date=%s"(tdate)
+    try:
+        conn = psycopg2.connect("dbname='lost' user='osnapdev' host='localhost'")
+    except:
+        print ("I am unable to connect to the database")
+    cur = conn.cursor()
+    cur.execute("""SELECT * FROM DC""")
+    rows = cur.fetchall()
+    for row in rows:
+        print ("   ", row[0])
+
+
+
 
 
 @app.route('/logout')
