5d65242c7b86d0311277a71fd48118954a0d4113
astraub2 <astraub2@uoregon.edu>
2017-02-05 20:14:26 -0800

    figured out input output

--- wsgi/app.py
+++ wsgi/app.py
@@ -53,12 +53,32 @@ def intransrep():
 @app.route('/inventoryrep', methods = ['POST', 'GET'])
 def inventoryrep():
     if request.method == 'POST':
-       idate = request.form['idate']
-       facility= request.form['facility']
-       resp = make_response(render_template('inventoryrep.html'))
-       resp.set_cookie('idate', idate)
-       resp.set_cookie('facility', facility)
-       invenbuilder(idate, facility)
+        idate = request.form['idate']
+        facility= request.form['facility']
+        resp.set_cookie('idate', idate)
+        resp.set_cookie('facility', facility)
+        invenbuilder(idate, facility)
+        command="SELECT assets.assets_pk, asset_at.facility_fk, assets.alt_description,asset_at.arrive_dt FROM asset_at LEFT JOIN assets ON asset_at.asset_fk=assets.assets_pk WHERE asset_at.facility_fk=(SELECT facilities_pk FROM facilities WHERE common_name='%s')"%(facility)
+        try:
+            conn = psycopg2.connect("dbname=lost host='/tmp/'")
+        except psycopg2.Error as e:
+            print ("I am unable to connect to the database")
+            print (e)
+            print (e.pgcode)
+            print (e.pgerror)
+            print (traceback.format_exc())
+            
+        cur = conn.cursor()
+        cur.execute(command)
+        res = cur.fetchall()  # this is the result of the database query "SELECT column_name1, column_name2 FROM some_table"
+        processed_data = []   # this is the processed result I'll stick in the session (or pass to the template)
+        for r in res:
+            processed_data.append( dict(zip(('column_name1', 'column_name2'), r)) )  # just making a dict out of the tuples from res
+        session['processed_data_session_name'] = processed_data
+        resp = make_response(render_template('inventoryrep.html'))
+   
+
+
    
     return resp
 
@@ -95,8 +115,12 @@ def invenbuilder(idate, facility):
         
     cur = conn.cursor()
     cur.execute(command)
-    rows = cur.fetchall()
-    print(rows)
+    res = cur.fetchall()  # this is the result of the database query "SELECT column_name1, column_name2 FROM some_table"
+    processed_data = []   # this is the processed result I'll stick in the session (or pass to the template)
+    for r in res:
+        processed_data.append( dict(zip(('column_name1', 'column_name2'), r)) )  # just making a dict out of the tuples from res
+    session['processed_data_session_name'] = processed_data
+    
 
 
 
