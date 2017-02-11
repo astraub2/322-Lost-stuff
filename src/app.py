204e4a462fb046ad3dd51b0dbbb06795b33f69b5
astraub2 <astraub2@uoregon.edu>
2017-02-11 15:38:24 -0800

    test server files

--- src/app.py
+++ src/app.py
@@ -1,4 +1,6 @@
 from flask import Flask,make_response, render_template, session, redirect, url_for, escape, request
+from config import dbname, dbhost, dbport, lost_priv, lost_pub, user_pub, prod_pub
+import json
 import psycopg2
 app = Flask(__name__)
 import traceback
@@ -186,6 +188,145 @@ def invenbuilder(idate, facility):
 @app.route('/logout')
 def logout():
     return render_template('logout.html')
+
+@app.route('/rest')
+
+def list_products():
+    """This function is huge... much of it should be broken out into other supporting
+        functions"""
+    
+    encrypted_request = False
+    # Check if the call uses crypto
+    if request.method=='POST' and 'signature' in request.form and \
+            request.form['signature'] != '' and 'arguments' in request.form:
+        
+        encrypted_request = True
+        # do the crypto, expect that inventory is on the other side
+        (data, skey, nonce) = decrypt_and_verify(request.form['arguments'], request.form['signature'], lost_priv, user_pub)
+        req=req=json.loads(data)
+    # Check maybe process as plaintext
+    elif request.method=='POST' and 'arguments' in request.form:
+        req=json.loads(request.form['arguments'])
+    # Unmatched, take the user somewhere else
+    else:
+        redirect('rest')
+    
+    # Setup a connection to the database
+    conn = psycopg2.connect(dbname=dbname,host=dbhost,port=dbport)
+    cur  = conn.cursor()
+    
+    # If execution gets here we have request json to work with
+    # Do I need to handle compartments in this query?
+    if len(req['compartments'])==0:
+        print("have not compartment")
+        # Just handle vendor and description
+        SQLstart = """select vendor,description,string_agg(c.abbrv||':'||l.abbrv,',')
+from products p
+left join security_tags t on p.product_pk=t.product_fk
+left join sec_compartments c on t.compartment_fk=c.compartment_pk
+left join sec_levels l on t.level_fk=l.level_pk"""
+        if req['vendor']=='' and req['description']=='':
+            # No filters, add the group by and query is ready to go
+            SQLstart += " group by vendor,description"
+            cur.execute(SQLstart)
+        else:
+            if not req['vendor']=='' and not req['description']=='':
+                req['vendor']="%"+req['vendor']+"%"
+                req['description']="%"+req['description']+"%"
+                SQLstart += " where description ilike %s and vendor ilike %s group by vendor,description"
+                cur.execute(SQLstart,(req['description'],req['vendor']))
+            elif req['vendor']=='':
+                req['description']="%"+req['description']+"%"
+                SQLstart += " where description ilike %s group by vendor,description"
+                cur.execute(SQLstart,(req['description'],))
+            elif req['description']=='':
+                req['vendor']="%"+req['vendor']+"%"
+                SQLstart += " where vendor ilike %s group by vendor,description"
+                cur.execute(SQLstart,(req['vendor'],))
+    else:
+        print("have compartment %s"%len(req['compartments']))
+        # Need to handle compartments too
+        SQLstart = """select vendor,description,string_agg(c.abbrv||':'||l.abbrv,',')
+from security_tags t
+left join sec_compartments c on t.compartment_fk=c.compartment_pk
+left join sec_levels l on t.level_fk=l.level_pk
+left join products p on t.product_fk=p.product_pk
+where product_fk is not NULL and c.abbrv||':'||l.abbrv = ANY(%s)"""
+        if req['vendor']=='' and req['description']=='':
+            # No filters, add the group by and query is ready to go
+            SQLstart += " group by vendor,description,product_fk having count(*)=%s"
+            cur.execute(SQLstart,(req['compartments'],len(req['compartments'])))
+        else:
+            if not req['vendor']=='' and not req['description']=='':
+                req['vendor']="%"+req['vendor']+"%"
+                req['description']="%"+req['description']+"%"
+                SQLstart += " and description ilike %s and vendor ilike %s group by vendor,description,product_fk having count(*)=%s"
+                cur.execute(SQLstart,(req['compartments'],req['description'],req['vendor'],len(req['compartments'])))
+            elif req['vendor']=='':
+                req['description']="%"+req['description']+"%"
+                SQLstart += " and description ilike %s group by vendor,description,product_fk having count(*)=%s"
+                cur.execute(SQLstart,(req['compartments'],req['description'],len(req['compartments'])))
+            elif req['description']=='':
+                req['vendor']="%"+req['vendor']+"%"
+                SQLstart += " and vendor ilike %s group by vendor,description,product_fk having count(*)=%s"
+                cur.execute(SQLstart,(req['compartments'],req['vendor'],len(req['compartments'])))
+    
+    # One of the 8 cases should've run... process the results
+    dbres = cur.fetchall()
+    listing = list()
+    for row in dbres:
+        e = dict()
+        e['vendor'] = row[0]
+        e['description'] = row[1]
+        if row[2] is None:
+            e['compartments'] = list()
+        else:
+            e['compartments'] = row[2].split(',')
+        listing.append(e)
+    
+    # Prepare the response
+    dat = dict()
+    dat['timestamp'] = req['timestamp']
+    dat['listing'] = listing
+    data = json.dumps(dat)
+    
+    # Return the data (encrypt or not depending on request
+    #if encrypted_request:
+        #data = encrypt(data,skey,nonce)
+    
+    conn.close()
+    return data
+    
+@app.route('/rest/suspend_user', methods=('POST',))
+def suspend_user():
+    # Check if the call uses crypto
+    if request.method=='POST' and 'signature' in request.form and \
+            request.form['signature'] != '' and 'arguments' in request.form:
+        # do the crypto, expect that hr is on the other side
+        #(data, skey, nonce) = decrypt_and_verify(request.form['arguments'], request.form['signature'], lost_priv, user_pub)
+        
+        # Process the request
+        req=json.loads(data)
+        
+        # Prepare the response data
+        dat = dict()
+        dat['timestamp'] = req['timestamp']
+        dat['result'] = 'OK'
+        data = json.dumps(dat)
+        
+        # Encrypt and send the response
+        #data = encrypt(data,skey,nonce)
+        #return data
+    
+    # Try to handle as plaintext
+    elif request.method=='POST' and 'arguments' in request.form:
+        req=json.loads(request.form['arguments'])
+
+    dat = dict()
+    dat['timestamp'] = req['timestamp']
+    dat['result'] = 'OK'
+    data = json.dumps(dat)
+    return data
 
 
 
