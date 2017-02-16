4a9da9169e28ef78dbafd3e25c00915017fd9979
astraub2 <astraub2@uoregon.edu>
2017-02-16 15:05:20 -0800

    start of create_tables

---
+++ create_tables.sql
@@ -0,0 +1,29 @@
+create table authentication
+(user_fk serial primary key,
+ username varchar(16),
+ password varchar(16),
+ logged_in BOOLEAN DEFAULT false);
+#I made user_pk and logged_in variables for future accesability
+#I limeted user name and password to 16 char according
+#to specification
+#following tables will likely come in handy:
+create table users
+(user_pk serial primary key,
+username varchar(255),
+password varchar(16),
+active BOOLEAN
+logged_in BOOLEAN DEFAULT false);
+
+create table roles
+(role_pk serial primary key,
+username varchar(255),
+active BOOLEAN);
+
+create table user_is
+(user_fk INT,
+role_fk integer REFERENCES roles (role_pk) not null DEFAULT 1);
+
+create table user_supports
+(user_fk integer REFERENCES users (user_pk) not null DEFAULT 1,
+facility_fk integer DEFAULT 1);
+
