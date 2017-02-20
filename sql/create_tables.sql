8b245569e410f701ac1020eb9744544b47fe8025
astraub2 <astraub2@uoregon.edu>
2017-02-19 17:31:42 -0800

    step and 5

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -1,8 +1,8 @@
 
-#I made user_pk and logged_in variables for future accesability
-#I limeted user name and password to 16 char according
-#to specification
-#following tables will likely come in handy:
+--I made user_pk and logged_in variables for future accesability
+--I limeted user name and password to 16 char according
+--to specification
+--following tables will likely come in handy:
 create table users
 (user_pk serial primary key,
 username varchar(16),
@@ -19,7 +19,7 @@ create table user_is
 (user_fk INT,
 role_fk integer REFERENCES roles (role_pk) not null DEFAULT 1);
 
-create table user_supports
-(user_fk integer REFERENCES users (user_pk) not null DEFAULT 1,
-facility_fk integer DEFAULT 1);
+-- create table user_supports
+-- (user_fk integer REFERENCES users (user_pk) not null DEFAULT 1,
+-- facility_fk integer DEFAULT 1);
 
