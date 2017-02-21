8c9c786369bf0572b5ed35b2cad5dc46ec70601b
astraub2 <astraub2@uoregon.edu>
2017-02-21 11:57:42 -0800

    create_tables update

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -7,19 +7,40 @@ create table users
 (user_pk serial primary key,
 username varchar(16),
 password varchar(16),
+role_fk integer REFERENCES roles (role_pk),
 active BOOLEAN,
 logged_in BOOLEAN);
 
+#roles table connects to users with role_fk
 create table roles
-(role_pk serial primary key,
-username varchar(255),
-active BOOLEAN);
+(
+role_pk serial primary key,
+role_name varchar(255),
+);
 
+#returns a table of what each users role is
+#may be superfluouse, but keep for now
 create table user_is
 (user_fk INT,
-role_fk integer REFERENCES roles (role_pk) not null DEFAULT 1);
+role_fk integer REFERENCES roles (role_pk),
+role_name varchar(255);
 
--- create table user_supports
--- (user_fk integer REFERENCES users (user_pk) not null DEFAULT 1,
--- facility_fk integer DEFAULT 1);
+create table assets
+(assets_pk int,
+ asset_tag varchar(16),
+ alt_description varchar(255));
+
+create table facilities
+(facilities_pk int,
+fcode varchar(6),
+common_name varchar(32),
+location varchar(255));
+
+#connects assets and facilities with dates to narrow results and 
+#show history of items
+create table asset_at
+	(asset_fk INT,
+		facility_fk integer,
+		arrive_dt TIMESTAMP,
+		depart_dt TIMESTAMP);
 
