e27b4dfe74d84343bba5ff8a36f9e7e0090caa6f
astraub2 <astraub2@uoregon.edu>
2017-02-21 12:40:15 -0800

    create_tables update

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -7,7 +7,7 @@ create table users
 (user_pk serial primary key,
 username varchar(16),
 password varchar(16),
-role_fk integer REFERENCES roles (role_pk),
+role_fk integer,
 active BOOLEAN,
 logged_in BOOLEAN);
 
