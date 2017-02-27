87dd0ecdf7f07c738d9a74761d15d1f31c93469a
astraub2 <astraub2@uoregon.edu>
2017-02-26 17:08:00 -0800

    testing step 7

--- src/app.py
+++ src/app.py
@@ -225,7 +225,7 @@ def dispose_asset():
                         except ProgrammingError:
                                 pk = None
                         #update dispose on asset and change asset_at
-                        cur.execute('UPDATE asset_at SET depart_dt=%s WHERE asset_fk=(SELECT assets_pk FROM assets WHERE asset_tag=%s)'(dispose_dt, asset_tag,))
+                        cur.execute('UPDATE asset_at SET depart_dt='%s' WHERE asset_fk=(SELECT assets_pk FROM assets WHERE asset_tag=%s)'(dispose_dt, asset_tag,))
                         return render_template('asset_disposed.html')
                         
         else:
