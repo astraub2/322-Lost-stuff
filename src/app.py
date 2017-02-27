16907310b592bbce0a2588ec534195902cd70592
astraub2 <astraub2@uoregon.edu>
2017-02-26 18:43:32 -0800

    testing step 7

--- src/app.py
+++ src/app.py
@@ -284,7 +284,7 @@ def asset_report():
                 facility = request.form['facility']
                 conn = psycopg2.connect(dbname=dbname, host=dbhost, port=dbport)
                 cur = conn.cursor()
-                if facility='':
+                if facility= None:
                         
                         cur.execute('SELECT a.asset_tag, a.alt_description, aa.arrive_dt, aa.depart_dt, \
                                 f.common_name, f.fcode FROM assets AS a INNER JOIN \
