9f1d70492ae1fb8317594c95915eec40a306f970
astraub2 <astraub2@uoregon.edu>
2017-03-01 17:15:50 -0800

    step 4 testing

--- src/app.py
+++ src/app.py
@@ -370,8 +370,8 @@ def transit_request():
                 
                 ##add transitrequest to DB
                 cur.execute('INSERT INTO transfer (asset_fk, requestor_fk, request_dt, source_fk, destination_fk) VALUES\
-                            ((SELECT asset_pk FROM assets WHERE asset_tag= %s), (SELECT user_pk FROM users WHERE username=%s),\
-                            %s, (SELECT facility_pk FROM facilities WHERE common_name=%s), (SELECT facility_pk FROM facilities WHERE common_name=%s))\
+                            ((SELECT assets.asset_pk FROM assets WHERE asset_tag= %s), (SELECT users.user_pk FROM users WHERE username=%s),\
+                            %s, (SELECT facilities.facility_pk FROM facilities WHERE common_name=%s), (SELECT facilities.facility_pk FROM facilities WHERE common_name=%s))\
                             ;', (asset_tag, username, date, source, destination))
                 conn.commit()
                 print('pass 3');
