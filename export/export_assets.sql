ce3c921ee8897c9c7e90f47da7450565f3ee28c8
astraub2 <astraub2@uoregon.edu>
2017-03-12 16:32:21 -0700

    export testing

--- export/export_assets.sql
+++ export/export_assets.sql
@@ -5,12 +5,11 @@
 SELECT assets.asset_tag, assets.alt_description, facilities.fcode, asset_at.arrive_dt,
  concat('NULL', assets.disposed_dt) AS disposed_dt FROM assets 
  INNER JOIN asset_at  ON assets.assets_pk=asset_at.asset_fk 
-INNER JOIN facilities AS f ON facilities.facilities_pk=asset_at.facility_fk 
-WHERE asset_at.arrive_dt=asset_at.intake_dt AND disposed_dt IS NULL;
+INNER JOIN facilities ON facilities.facilities_pk=asset_at.facility_fk 
+WHERE assets.disposed_dt IS NULL;
 
 SELECT assets.asset_tag, assets.alt_description, facilities.fcode, asset_at.arrive_dt
 , assets.disposed_dt FROM assets INNER JOIN asset_at  ON 
 assets.assets_pk=asset_at.asset_fk INNER JOIN facilities  ON 
 facilities.facilities_pk=asset_at.facility_fk WHERE 
-asset_at.arrive_dt=assets.intake_dt 
-AND disposed_dt IS NOT NULL;
+assets.disposed_dt IS NOT NULL;
--- export/export_users.sql
+++ export/export_users.sql
@@ -1,3 +1,3 @@
 \echo 'username,password,role,active'
-SELECT users.username, users.password, roles.role_name, users.a
+SELECT users.username, users.password, roles.role_name, users.active
  FROM users LEFT JOIN roles ON role_fk=role_pk;
