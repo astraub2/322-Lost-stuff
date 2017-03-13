6e699e4d75b8d8a362f794c237ec3e7d0bec3b39
astarub2 <astraub2@uoregon.edu>
2017-03-13 00:32:48 +0000

    files

---
+++ import/lost_data/import_data.sql
@@ -0,0 +1,17 @@
+INSERT INTO facilities (fcode, common_name) VALUES ('HQ', 'Headquarters');
+INSERT INTO facilities (fcode, common_name) VALUES ('DC', 'Capital');
+INSERT INTO facilities (fcode, common_name) VALUES ('M0A', 'Alpha Site');
+INSERT INTO facilities (fcode, common_name) VALUES ('M0B', 'Beta Site');
+INSERT INTO facilities (fcode, common_name) VALUES ('M0G', 'Gamma Site');
+INSERT INTO facilities (fcode, common_name) VALUES ('P4OM', 'Olympus');
+INSERT INTO facilities (fcode, common_name) VALUES ('P4HB', 'Hellas Basin');
+INSERT INTO facilities (fcode, common_name) VALUES ('G001', 'Abydos');
+you didnt do this
+you didnt do this
+you didnt do this
+you didnt do this
+you didnt do this
+you didnt do this
+you didnt do this
+you didnt do this
+you didnt do this
