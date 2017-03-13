067967ecbf7184c5d69bf2a67d85538fb401d14e
astraub2 <astraub2@uoregon.edu>
2017-03-12 18:49:50 -0700

    python script transit testing

--- import/import_data.py
+++ import/import_data.py
@@ -34,9 +34,26 @@ def import_assets():
                                 print("INSERT INTO asset_at (asset_fk, facility_fk, arrive_dt) VALUES\
                                       ((SELECT assets_pk FROM assets WHERE asset_tag='{}'), (SELECT\
                                       facilities_pk FROM facilities WHERE fcode='{}'), '{}');".format(s[0], s[2], s[3]))
+def import_transfers():
+	with open("transfers.csv") as f:
+		transfers = csv.reader(f, skipinitialspace=True)
+		next(transfers)
+		for s in transfers:
+			print("""INSERT INTO transfer (requestor_fk, request_dt, approver_fk, approval_dt, source_fk, destination_fk, asset_fk) VALUES 
+				((SELECT user_pk FROM users WHERE username='{}'), '{}', (SELECT user_pk FROM users WHERE username='{}'), 
+				'{}', (SELECT facilities_pk FROM facilities WHERE fcode='{}'), 
+				(SELECT facilities_pk FROM facilities WHERE fcode='{}'), (SELECT assets_pk FROM assets WHERE asset_tag='{}'));
+				""".format(s[1], s[2], s[3], s[4], s[5], s[6], s[0]))
+			print("""INSERT INTO transit (transfer_fk, asset_fk, load_dt, unload_dt) VALUES ((SELECT transfer_pk FROM transfer WHERE asset_fk=(SELECT assets_pk FROM assets WHERE asset_tag='{}') 
+				AND request_dt=(SELECT max(request_dt) FROM transfer WHERE asset_fk=(SELECT asset_pk FROM assets WHERE asset_tag='{}'))), 
+				(SELECT assets_pk FROM assets WHERE asset_tag='{}'), '{}', '{}');""".format(s[0], s[0], s[0], s[7], s[8]))
+			print("UPDATE asset_at SET depart_dt='{}' WHERE asset_fk=(SELECT assets_pk FROM assets WHERE asset_tag='{}') AND depart_dt IS NULL;".format(s[7], s[0]))
+			print("""INSERT INTO asset_at (asset_fk, facility_fk, arrive_dt) VALUES ((SELECT asset_pk FROM assets WHERE asset_tag='{}'), 
+				(SELECT facility_pk FROM facilities WHERE facility_fcode='{}'), '{}');""".format(s[0], s[6], s[8]))
+
                         
 if __name__ == "__main__":
         import_facilities()
         import_assets()
         import_users()
-        #import_transfers()
+        import_transfers()
