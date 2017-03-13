6e710ed2723efdf5af9efad53aeb58f2984d8f30
astraub2 <astraub2@uoregon.edu>
2017-03-12 17:53:20 -0700

    python script for import

--- import/import_data.py
+++ import/import_data.py
@@ -26,7 +26,7 @@ def import_assets():
                               VALUES ({}, '{}', {});".format(s[0], s[1], s[4]))
                         print("INSERT INTO asset_at (asset_fk, facility_fk, arrive_dt) VALUES\
                               ((SELECT assets_pk FROM assets WHERE asset_tag='{}'), (SELECT\
-                              facilities_pk FROM facilities WHERE fcode={}), {});".format(s[0], s[2], s[3]))
+                              facilities_pk FROM facilities WHERE fcode='{}'), {});".format(s[0], s[2], s[3]))
 if __name__ == "__main__":
         import_facilities()
         import_assets()
