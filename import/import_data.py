7b7b6b1e5d2e295875642db0afebc7ac33ebe85a
astraub2 <astraub2@uoregon.edu>
2017-03-12 17:16:35 -0700

    fix to export shell script

--- import/import_data.py
+++ import/import_data.py
@@ -6,7 +6,7 @@ def import_facilities():
                 facilities = csv.reader(f, skipinitialspace=True)
                 next(facilities)
                 for s in facilities:
-                        print("INSERT INTO facilities (fcode, common_name) VALUES ({}, {});".format(s[0], s[1]))
+                        print("INSERT INTO facilities (fcode, common_name) VALUES ('{}', '{}');".format(s[0], s[1]))
 
 def import_users():
         with open("users.csv") as f:
@@ -21,7 +21,7 @@ def import_assets():
                 assets = csv.reader(f, skipinitialspace=True)
                 next(assets)
                 for s in assets:
-                        print("INSERT INTO assets (asset_tag, alt_description, disposed_dt) VALUES ({}, {}, {}, {});".format(s[0], s[1], s[4]))
+                        print("INSERT INTO assets (asset_tag, alt_description, disposed_dt) VALUES ({}, '{}', {});".format(s[0], s[1], s[4]))
                         print("INSERT INTO asset_at (asset_fk, facility_fk, arrive_dt) VALUES ((SELECT asset_pk FROM assets WHERE asset_tag={}), (SELECT facilities_pk FROM facilities WHERE fcode={}), {});".format(s[0], s[2], s[3]))
 if __name__ == "__main__":
         import_facilities()
