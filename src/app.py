be5b32a21701118d49a2dfb484a46c30a304269a
astraub2 <astraub2@uoregon.edu>
2017-03-19 21:18:44 -0700

    activate fix

--- src/app.py
+++ src/app.py
@@ -6,91 +6,44 @@ app = Flask(__name__)
 
 app.config["SECRET_KEY"] = secret_key
 
-# @app.route('/create_user', methods = ['GET', 'POST'])
-# def create_user():
-# 	if request.method == 'POST':
-# 		conn = psycopg2.connect(dbname=dbname, host=dbhost, port=dbport)
-# 		cur = conn.cursor()
-# 		username = request.form['username']
-# 		password = request.form['password']
-# 		role=request.form['role']
-# 		cur.execute('SELECT username FROM users WHERE username=%s', (username,))
-# 		try:
-# 			result = cur.fetchone()
-# 		except ProgrammingError:
-# 			result = None
 
-# 		if result == None:
-# 			#also add role if not in roles
-# 			cur.execute('SELECT role_pk FROM roles WHERE role_name=%s', (role,))
-# 			try:
-# 				results = cur.fetchone()
-# 			except ProgrammingError:
-# 				results = None
-# 			if results == None:
-# 				#role is not in table
-# 				cur.execute('INSERT INTO roles (role_name) VALUES (%s)', (role,))
-# 				cur.execute('SELECT role_pk FROM roles WHERE role_name=%s', (role,))
-# 				try:
-# 					role_pk = cur.fetchone()
-# 				except ProgrammingError:
-# 					role_pk = None
-# 				if role_pk != None:
-# 					cur.execute('INSERT INTO users (username, password, role_fk) VALUES (%s, %s, %s)', (username, password, role_pk))
-# 					conn.commit()
-# 					cur.close()
-# 					conn.close()
-# 					return render_template('user_added.html')
-# 			else:	
-# 				#role is in table, results=role_pk
-# 				cur.execute('INSERT INTO users (username, password, role_fk) VALUES (%s, %s, %s)', (username, password, results))
-# 				conn.commit()
-# 				cur.close()
-# 				conn.close()
-# 				return render_template('user_added.html')
-			
-# 		else:
-# 			cur.close()
-# 			conn.close()
-# 			return render_template('user_exists.html')
-
-# 	return render_template('create_user.html')
 @app.route('/activate_user', methods = ['POST',])
 def activate_user():
     if request.method == 'POST':
         print("reached me")
-        conn = psycopg2.connect(dbname=dbname, host=dbhost, port=dbport)
-        cur = conn.cursor()
-        username = request.form['username']
-        password = request.form['password']
-        role = request.form['role']
-        cur.execute('SELECT username FROM users WHERE username=%s;', (username,))
-        try:
-            result = cur.fetchone()
-        except ProgrammingError:
-            result = None
-
-        dat = dict()
-
-        if result == None:
-            cur.execute('INSERT INTO users (username, password) VALUES (%s, %s);', (username, password))
-            cur.execute('INSERT INTO user_is (role_fk, user_fk) VALUES ((SELECT role_pk FROM roles WHERE role_name=%s), (SELECT user_pk FROM users WHERE username=%s));', (role, username))
-            conn.commit()
-            cur.close()
-            conn.close()
-            returnValue = ('User %s Added, password: %s role :%s'%(username, password, role))
-            dat['result'] = returnValue
-            data = json.dumps(dat)
-            return data
-        else:
-            cur.execute('UPDATE users SET password=%s, active=%s WHERE username=%s;', (password, True, username))
-            conn.commit()
-            cur.close()
-            conn.close()
-            returnValue = ('User %s Activated, new password: %s'%(username, password))
-            dat['result'] = returnValue
-            data = json.dumps(dat)
-            return data
+        return 0;
+##        conn = psycopg2.connect(dbname=dbname, host=dbhost, port=dbport)
+##        cur = conn.cursor()
+##        username = request.form['username']
+##        password = request.form['password']
+##        role = request.form['role']
+##        cur.execute('SELECT username FROM users WHERE username=%s;', (username,))
+##        try:
+##            result = cur.fetchone()
+##        except ProgrammingError:
+##            result = None
+##
+##        dat = dict()
+##
+##        if result == None:
+##            cur.execute('INSERT INTO users (username, password) VALUES (%s, %s);', (username, password))
+##            cur.execute('INSERT INTO user_is (role_fk, user_fk) VALUES ((SELECT role_pk FROM roles WHERE role_name=%s), (SELECT user_pk FROM users WHERE username=%s));', (role, username))
+##            conn.commit()
+##            cur.close()
+##            conn.close()
+##            returnValue = ('User %s Added, password: %s role :%s'%(username, password, role))
+##            dat['result'] = returnValue
+##            data = json.dumps(dat)
+##            return data
+##        else:
+##            cur.execute('UPDATE users SET password=%s, active=%s WHERE username=%s;', (password, True, username))
+##            conn.commit()
+##            cur.close()
+##            conn.close()
+##            returnValue = ('User %s Activated, new password: %s'%(username, password))
+##            dat['result'] = returnValue
+##            data = json.dumps(dat)
+##            return data
 
 @app.route('/revoke_user', methods = ['POST',])
 def revoke_user():
